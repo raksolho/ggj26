61b23612bf84b6e6f9d79ff97bb99ce1ca505b16
Raks <raquelolho@hotmail.com>
2026-01-25 21:17:41 -0300

    placeholder animation

--- ggj26/Assets/Scripts/Movement.cs
+++ ggj26/Assets/Scripts/Movement.cs
@@ -5,12 +5,13 @@ public class Movement : MonoBehaviour
 {
     private PlayerInputActions input;
     private Vector2 moveInput;
-
+    private Animator animator;
     public float speed = 5f;
 
     void Awake()
     {
         input = new PlayerInputActions();
+        animator = GetComponent<Animator>();
     }
 
     void OnEnable()
@@ -29,5 +30,34 @@ public class Movement : MonoBehaviour
 
         Vector3 movement = new Vector3(moveInput.x, moveInput.y, 0);
         transform.Translate(movement * speed * Time.deltaTime);
+        UpdateAnimation();
+        
     }
+
+
+   void UpdateAnimation()
+{
+    Vector2 dir = moveInput  ;
+    if (dir.magnitude < 0.15f)
+        return;
+
+    animator.SetBool("WalkFront", false);
+    animator.SetBool("WalkBack", false);
+    animator.SetBool("WalkSide", false);
+
+    if (Mathf.Abs(dir.y) >= Mathf.Abs(dir.x) * 1.2f)
+    {
+        if (dir.y > 0)
+            animator.SetBool("WalkBack", true);
+        else
+            animator.SetBool("WalkFront", true);
+    }
+    else
+    {
+        animator.SetBool("WalkSide", true);
+        transform.localScale = new Vector3(dir.x > 0 ? 1 : -1, 1, 1);
+    }
+}
+
+    
 }
