f0e8e3c299502595e934a3bc26f9b84c22208e72
Raks <raquelolho@hotmail.com>
2026-01-26 22:06:58 -0300

    אקסא צשמשעקר

---
+++ ggj26/Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dialogue", menuName = "Dialogue", order = 0)]
+public class Dialogue : ScriptableObject
+{
+
+
+
+    public string name;
+
+
+    public string[] sentences;
+
+}
