0f30c6b3e73c51f0dbf2fbed9bea6a62b8f2e173
Nagib Moura Suaid <nagibmoura@gmail.com>
2026-01-29 22:12:07 -0300

    fix mask change logic

--- ggj26/Assets/Scripts/Masks/MaskAnimator.cs
+++ ggj26/Assets/Scripts/Masks/MaskAnimator.cs
@@ -1,44 +1,13 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-[RequireComponent(typeof(MaskCarrier))]
-public class MaskAnimator : MonoBehaviour
-{
-    MaskCarrier maskCarrier;
-    MaskCarrier recipient;
-    private List<Animator> animators;
 
-    void Start()
-    {
-        maskCarrier = GetComponent<MaskCarrier>();
+[RequireComponent(typeof(Animator))]
+public class MaskAnimator : MonoBehaviour {
 
-        animators = new List<Animator>(GetComponentsInChildren<Animator>(true));
-
-    }
-
-
-    public void HandleMaskRemove()
-    {
-        maskCarrier.SwapMask(recipient);
-    }
-    public void SetNextRecipient(MaskCarrier maskCarrier)
-    {
-        recipient = maskCarrier;
-
-    }
-    public void RemoveMaskAnimation()
-    {
-            SetTriggerForAllAnimators("removeMask");
-
-    }
-    private void SetTriggerForAllAnimators(string param)
-    {
-        foreach (Animator animator in animators)
-        {
-            if(animator.enabled && animator.gameObject.activeInHierarchy)
-            {
-                animator.SetTrigger(param);
-            }
-        }
-    }
+	[HideInInspector]
+	public Animator animator;
+	public Mask mask;
+	void OnEnable()
+	{
+		animator = GetComponent<Animator>();
+	}
 }
