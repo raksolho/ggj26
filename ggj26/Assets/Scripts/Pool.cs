a3c97026fd0846bf80e9e8dd1cc422fe8129bda4
Nagib Moura Suaid <nagibmoura@gmail.com>
2026-02-01 01:04:53 -0300

    fix pool and swim sound

--- ggj26/Assets/Scripts/Pool.cs
+++ ggj26/Assets/Scripts/Pool.cs
@@ -6,20 +6,35 @@ public class Pool : MonoBehaviour
     public GameObject swimmingSprite;
     public GameObject walkingSprite;
 
+    public static int poolCount = 0;
 
-    private void OnTriggerEnter2D(Collider2D other){
-        if(other.tag == "Player" && player.currentMask?.name == "WaterMask"){
-            swimmingSprite.GetComponent<SpriteRenderer>().enabled = true;
-            walkingSprite.GetComponent<SpriteRenderer>().enabled = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player" && player.currentMask?.name == "WaterMask")
+        {
+            poolCount += 1;
+            if (poolCount >= 1)
+            {
+                swimmingSprite.GetComponent<SpriteRenderer>().enabled = true;
+                walkingSprite.GetComponent<SpriteRenderer>().enabled = false;
+            }
+
         }
     }
     private void OnTriggerExit2D(Collider2D other)
     {
-        if(other.tag == "Player" && player.currentMask?.name == "WaterMask"){
-            swimmingSprite.GetComponent<SpriteRenderer>().enabled = false;
-            walkingSprite.GetComponent<SpriteRenderer>().enabled = true;
+        if (other.tag == "Player" && player.currentMask?.name == "WaterMask")
+        {
+            poolCount -= 1;
+            if (poolCount <= 0)
+            {
+                swimmingSprite.GetComponent<SpriteRenderer>().enabled = false;
+                walkingSprite.GetComponent<SpriteRenderer>().enabled = true;
+
+            }
         }
-        
+
     }
 
 }
--- ggj26/Assets/Scripts/Step.cs
+++ ggj26/Assets/Scripts/Step.cs
@@ -6,21 +6,25 @@ public class Step : MonoBehaviour
     AudioSource audioSource;
     public AudioClip stepSound;
     public AudioClip swimSound;
-    
+    SpriteRenderer spriteRenderer;
+
     void Awake()
     {
         audioSource = GetComponentInParent<AudioSource>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     public void StepNoise()
     {
+        if (spriteRenderer.enabled == false) return;
         audioSource.pitch = Random.Range(0.8f, 1.2f);
         audioSource.PlayOneShot(stepSound);
     }
 
     public void SwimNoise()
     {
+        if (spriteRenderer.enabled == false) return;
         audioSource.pitch = Random.Range(0.8f, 1.2f);
-        audioSource.PlayOneShot(swimSound);        
+        audioSource.PlayOneShot(swimSound);
     }
 }
